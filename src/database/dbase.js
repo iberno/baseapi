6c6a6e8caf955467b4bc531826998015b1115210
Iberno <iberno@outlook.com>
2020-01-25 16:57:53 -0300

    Start a Project of Base API with JWT

---
+++ src/database/dbase.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://taskman:MhFUexmiQzr4XK2J@cluster0-sxuij.mongodb.net/taskman?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
